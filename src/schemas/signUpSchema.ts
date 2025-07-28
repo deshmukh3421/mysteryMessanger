bd20b74826106a5efb601fa4a0f15df48c59850f
deshmukh3421 <udaydeshmukh1864@gmail.com>
2025-07-29 00:37:33 +0530

    Setup message generation and get it to send

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,7 +4,7 @@ export const usernameValidation = z
     .string()
     .min(3, "Min 2 charecters required in Username")
     .max(20, "Username can't be of more than 20 characters")
-    .regex(/^[a-zA-Z0-9_]$/,"Username can't have special characters")
+    .regex(/^[a-zA-Z0-9_]+$/,"Username can't have special characters")
 
 
 export const signUpSchema = z.object({
